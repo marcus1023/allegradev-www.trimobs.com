071b2a5f5fe9c27e281908e66c7ef33e7a21e9af
Marcus Ogden <marcus@userlite.com>
2017-01-11 10:27:45 -0700

    angular added

---
+++ js/app.js
@@ -0,0 +1,13 @@
+angular.module('trimobile',  ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        .state('home',{
+            url:'/home',
+            templateUrl: "../templates/home-tmpl.html",
+            controller: "mainController"
+        })
+
+
+        $urlRouterProvider
+            .otherwise('/');
+    });
