f25e0cf40fe991f26143469f1a592661e5057ed4
Marcus Ogden <marcus@userlite.com>
2017-01-12 13:02:32 -0700

    firefox fix

---
+++ js/directives/home-img-title.js
@@ -0,0 +1,6 @@
+angular.module('trimobile').directive('testing', function(){
+  return {
+    restrict: 'AE',
+    templateUrl: '../templates/home-img-title.html'
+  }
+});
---
+++ js/directives/page-links-dir.js
@@ -0,0 +1,6 @@
+angular.module('trimobile').directive('pageLinks', function(){
+  return {
+    restrict: 'AE',
+    templateUrl: '../templates/page-links-tmpl.html'
+  }
+});
